1c6d15350c10ee9fa6f530a1307bcaf59d2d3e58
Iraj Eshghi <irajeshghi@gmail.com>
2017-09-06 12:46:53 -0400

    right-biased riemann sum implemented. Tested with one function, converges

--- riemann.py
+++ riemann.py
@@ -6,10 +6,18 @@
 # which has default value of the empty string. If the user calls 
 # hello_world() without an argument, then the compiler uses ''
 # as the default value of the argument.
-def hello_world(name=''):
-    print "hello world!"
-    print name
-    return
-    
-    
 #Implement the Riemann Sum approximation for integrals.
+
+import numpy as np
+
+def func(x): #write here whatever function we want to integrate
+    return np.sin(x)
+
+
+def riemann(a,b,n): #implement right-biased riemann sum between endpoints a and b with n rectangles
+    vfunc = np.vectorize(func)
+    arr = np.linspace(a,b,n)
+    dx = arr[1]-arr[0]
+    rsum = sum(dx*vfunc(arr+dx)) #we add dx to all points to make it right-biased
+    return rsum
+
